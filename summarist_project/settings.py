2e779ef8e89e0d30b6e4a3d0b5a5d2b64970a17d
M4D-Programmer <donaven1022@gamil.com>
2026-08-21 19:25:48 -0400

    db

--- summarist_project/settings.py
+++ summarist_project/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'summarist_project.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         # Replace this value with your local database's connection string.
-        default='postgresql://postgres:postgres@localhost:5432/summarist_project',
+        default='postgresql://postgres:postgres@localhost:5432/summarist_db',
         conn_max_age=600
     )
 }
